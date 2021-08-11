76b56ce5324ee84d9102c4ced91f17c59ff6aae4
panggggg <p.picczy@gmail.com>
2021-08-11 22:22:36 +0700

    Add example for spread array

--- Array/spread_array.js
+++ Array/spread_array.js
@@ -1,5 +1,15 @@
 const number = [1, 2, 3, 4]
 
-const newNumber = [...number, 5]
+const newNumber = [...number, 5] // copy array
 
 console.log('newNumber: ', newNumber)
+
+//----------------------------------------------
+
+const array_number = [1, 2, 3, 4, 5]
+const new_array_number = array_number // same address
+
+new_array_number.push(8)
+
+console.log('array_number: ', array_number) //[ 1, 2, 3, 4, 5, 8 ] 
+console.log('new_array_number: ', new_array_number) //[ 1, 2, 3, 4, 5, 8 ] 
